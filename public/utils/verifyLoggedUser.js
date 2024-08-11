c34286607d4a895a08d90ca0c7e9d64cd37dcb44
Sebastian <schaquila@autonoma.edu.pe>
2024-08-10 23:05:10 -0500

    Refactor login and redirect URLs to remove unnecessary "public" folder

--- public/utils/verifyLoggedUser.js
+++ public/utils/verifyLoggedUser.js
@@ -3,6 +3,7 @@ function checkLoginStatus() {
   const username = localStorage.getItem("username");
 
   if (!isLogged || !username) {
+    console.log("redirecting to login page");
     window.location.href = "../Login/index.html";
   }
 }
